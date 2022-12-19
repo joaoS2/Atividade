3be2607434ca2ab3bd83c5a699343ce6962f2c30
João Victor S2 <112714646+joaoS2@users.noreply.github.com>
2022-12-19 12:21:54 -0300

    Delete questao22.py

--- Atividade do Livro/Cap 4/questao22.py
+++
@@ -1,8 +0,0 @@
-# Este programa solicita ao usuarío suda idade e seu peso e mostra a categoria que ele se encaixa de acordo com uma tabela. */
-
-
-#|Dfinição de Variávies|
-
-idade : float
-peso : float
-
